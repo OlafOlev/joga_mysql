fbd176489a4dc6afe553cf7ef450da6f79c11142
OlafOlev <oalfolev@gmail.com>
2023-06-10 11:06:36 +0300

    Installed basic packages Created project basic files Added handlebars template engine Added static css and image files Displayed all articles Displayed an article by slug Added author name into single article Displayed author article list Created article controller and route, refactored application code

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+
+const path = require('path')
+const hbs = require('express-handlebars')
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.engine('hbs', hbs.engine({
+    extname: 'hbs',
+    defaultLayout: 'main',
+    layoutsDir: __dirname + '/views/layouts/',
+}))
+
+app.use(express.static('public'))
+
+const mysql = require('mysql')
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: true}))
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "qwerty",
+    database: "joga_mysql",
+})
+
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("connected to joga_mysql db")
+})
+const articleRoutes = require('./routes/article');
+const authorRoutes = require('./routes/author')
+
+app.use('/', articleRoutes)
+app.use('/article', articleRoutes)
+
+app.use('/author', authorRoutes)
+app.listen(3000, () => {
+    console.log("App is started at http://localhost:3000")
+})
